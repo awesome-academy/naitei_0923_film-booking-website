960e519df63186d99a8366e42993d4c87b683597
vutuankiet4599 <vutuankiet4599@gmail.com>
2023-09-11 21:32:00 +0700

    init project with package breeze, localization, tailwindcss; create admin layout; user use app layout

---
+++ resources/views/test.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <div class="text-red-500">Hello test</div>
+</x-app-layout>
